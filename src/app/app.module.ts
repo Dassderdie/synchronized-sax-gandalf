c94a92575b4b082ddd36f99c80cce3f15c4c91b4
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2021-07-15 21:45:26 +0200

    Add PWA (to hopefully add github-pages support for SPA)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageModule } from './components/landing-page/landing-page.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
@@ -13,6 +15,12 @@ import { LandingPageModule } from './components/landing-page/landing-page.module
         AppRoutingModule,
         BrowserAnimationsModule,
         LandingPageModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the app is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent],
