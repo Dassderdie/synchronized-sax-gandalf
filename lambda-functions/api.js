ec72c8522ecc917dee91b8d9d36f08427f7b97c7
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2021-07-29 18:30:03 +0200

    Refactor project to monorepo

---
+++ lambda-functions/api.js
@@ -0,0 +1,14 @@
+import Pusher from 'pusher';
+
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: process.env.PUSHER_CLUSTER,
+});
+
+export default (req, res) => {
+    const auth = pusher.authenticate(req.body.socket_id, req.body.channel_name);
+
+    res.send(auth);
+};
