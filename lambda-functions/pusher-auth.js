43a66c1643912f576cc66cb30e9ca55b8ffa2869
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2021-08-01 17:50:07 +0200

    Add logging for pusher-auth function

--- lambda-functions/pusher-auth.js
+++ lambda-functions/pusher-auth.js
@@ -10,10 +10,10 @@ const pusher = new Pusher({
 
 // I used https://github.com/NathanHeffley/pointer/blob/f12442f416ae9aa3929482142650b358bf50c1ca/src/functions/auth.js as inspiration
 exports.handler = function (event, context, callback) {
-    const query = event.queryStringParameters;
-    const auth = JSON.stringify(
-        pusher.authenticate(query.socket_id, query.channel_name)
-    );
+    console.log(event, event.queryStringParameters);
+    const socketId = event.body.socket_id;
+    const channel = event.body.channel_name;
+    const auth = JSON.stringify(pusher.authenticate(socketId, channel));
     const authCallback = query.callback.replace(/\"/g, '') + '(' + auth + ');';
 
     callback(null, {
