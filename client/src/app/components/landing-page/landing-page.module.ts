6e480c4ef99ddde894589cc4f6a1a1a61862cb15
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2021-11-23 17:36:44 +0100

    Remove unused code

--- client/src/app/components/landing-page/landing-page.module.ts
+++ client/src/app/components/landing-page/landing-page.module.ts
@@ -3,16 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { ChannelModule } from 'src/app/shared/channel/channel.module';
 
 @NgModule({
     declarations: [LandingPageComponent],
-    imports: [
-        CommonModule,
-        LandingPageRoutingModule,
-        MatExpansionModule,
-        ChannelModule,
-    ],
+    imports: [CommonModule, LandingPageRoutingModule, ChannelModule],
 })
 export class LandingPageModule {}
